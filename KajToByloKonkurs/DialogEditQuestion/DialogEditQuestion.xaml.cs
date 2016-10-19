e5d9e9a2e4e175dd96a5bdf3dcf856aaea412ac0
teodorxxx <tdrzyzga@gmail.com>
2016-10-19 21:29:17 +0200

    add window edit question

---
+++ KajToByloKonkurs/DialogEditQuestion/DialogEditQuestion.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KajToBylo
+{
+    /// <summary>
+    /// Interaction logic for DialogEditQuestion.xaml
+    /// </summary>
+    public partial class DialogEditQuestion : Window
+    {
+        public bool DialogEditQuestionResult { get; private set; }
+        public QuestionAnswers Question { get; private set; }
+
+        private MainWindow.IndexCategory Category;
+        private MainWindow.CorrectAnswer CorrectAnswer;
+
+        public DialogEditQuestion(MainWindow.IndexCategory category, QuestionAnswers question)
+        {
+            InitializeComponent();
+
+            Question = question;
+            Category = category;
+            CorrectAnswer = Question.GoodAnswer;
+
+            editQuestion.Text = question.Question;
+            dialogEditQuestionAnswerA.Text = Question.ResponseA;
+            dialogEditQuestionAnswerB.Text = Question.ResponseB;
+            dialogEditQuestionAnswerC.Text = Question.ResponseC;
+            dialogEditQuestionAnswerD.Text = Question.ResponseD;
+
+            SetCategory();
+            SetCorrectAnswer();
+        }
+
+        private void dialogEditQuestionSave_Click(object sender, RoutedEventArgs e)
+        {
+            DialogEditQuestionResult = true;
+
+            QuestionAnswers questionAfterChanges = new QuestionAnswers(editQuestion.Text, CheckCorrectAnswer(), dialogEditQuestionAnswerA.Text, dialogEditQuestionAnswerB.Text, dialogEditQuestionAnswerC.Text, dialogEditQuestionAnswerD.Text, DateTime.Now);
+            Question = questionAfterChanges;
+
+            this.Hide();
+        }
+
+        private void SetCategory()
+        {
+            switch (Category)
+            {
+                case MainWindow.IndexCategory.MusicPL:
+                    dialogEditQuestionMusicPL.IsChecked = true;
+                    break;
+                case MainWindow.IndexCategory.MusicSL:
+                    dialogEditQuestionMusicSL.IsChecked = true;
+                    break;
+                case MainWindow.IndexCategory.Movie:
+                    dialogEditQuestionMovie.IsChecked = true;
+                    break;
+                case MainWindow.IndexCategory.Book:
+                    dialogEditQuestionBook.IsChecked = true;
+                    break;
+            }
+        }
+
+        private void SetCorrectAnswer()
+        {
+            switch (CorrectAnswer)
+            {
+                case MainWindow.CorrectAnswer.AnswerA:
+                    dialogEditQuestionA.IsChecked = true;
+                    break;
+                case MainWindow.CorrectAnswer.AnswerB:
+                    dialogEditQuestionB.IsChecked = true;
+                    break;
+                case MainWindow.CorrectAnswer.AnswerC:
+                    dialogEditQuestionC.IsChecked = true;
+                    break;
+                case MainWindow.CorrectAnswer.AnswerD:
+                    dialogEditQuestionD.IsChecked = true;
+                    break;
+            }
+        }
+
+
+        private MainWindow.IndexCategory CheckCategory()
+        {
+            if (dialogEditQuestionMusicPL.IsChecked == true)
+                return MainWindow.IndexCategory.MusicPL;
+            else if (dialogEditQuestionMusicSL.IsChecked == true)
+                return MainWindow.IndexCategory.MusicSL;
+            else if (dialogEditQuestionMovie.IsChecked == true)
+                return MainWindow.IndexCategory.Movie;
+            else
+                return MainWindow.IndexCategory.Book;
+        }
+
+        private MainWindow.CorrectAnswer CheckCorrectAnswer()
+        {
+            if (dialogEditQuestionA.IsChecked == true)
+                return MainWindow.CorrectAnswer.AnswerA;
+            else if (dialogEditQuestionB.IsChecked == true)
+                return MainWindow.CorrectAnswer.AnswerB;
+            else if (dialogEditQuestionC.IsChecked == true)
+                return MainWindow.CorrectAnswer.AnswerC;
+            else
+                return MainWindow.CorrectAnswer.AnswerD;
+        }
+
+        private void dialogEditQuestionCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
